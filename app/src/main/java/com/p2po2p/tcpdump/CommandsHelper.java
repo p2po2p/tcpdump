6133da573a2e90bf13fbfcd0d3a81d39b40a8f75
p2po2p <p2po2p@gmail.com>
2018-01-15 15:57:58 +0800

    tcpdump指定 any

--- app/src/main/java/com/p2po2p/tcpdump/CommandsHelper.java
+++ app/src/main/java/com/p2po2p/tcpdump/CommandsHelper.java
@@ -31,6 +31,83 @@ public class CommandsHelper {
     public static String FILE_NAME = "capture.pcap";
 
     public static boolean startCapture(Context context) {
+        Log.i("hqr", "startCapture start");
+        //准备tcpdump文件+赋予tcpdump权限
+        prepareTCPDumpFile(context);
+        //获取any网卡的id
+        int num = tcpdump_d_any();
+        Log.i("hqr", "startCapture num:" + num);
+        boolean retVal = false;
+        try {
+            String[] commands = new String[7];
+            commands[0] = "adb shell";
+            commands[1] = "su";
+            commands[5] = "cd /data/local";
+            commands[6] = "tcpdump -i "+ num +" -p -vv -s 0 -w " + DEST_FILE;
+            execCmd(commands);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Log.i("hqr", "startCapture end");
+        return retVal;
+    }
+
+    public static void stopCapture(Context context) {
+        Log.i("hqr", "stopCapture start");
+        // 找出所有的带有tcpdump的进程
+        String[] commands = new String[2];
+        commands[0] = "adb shell";
+        commands[1] = "ps|grep tcpdump|grep root|awk '{print $2}'";
+        Process process = execCmd(commands);
+        String result = parseInputStream(process.getInputStream());
+        if (!TextUtils.isEmpty(result)) {
+            String[] pids = result.split("\n");
+            String[] killCmds = new String[pids.length];
+            for (int i = 0; i < pids.length; ++i) {
+                killCmds[i] = "kill -9 " + pids[i];
+            }
+            execCmd(killCmds);
+        }
+        Log.i("hqr", "stopCapture end");
+    }
+
+    /**
+     * 执行TCPDUMP -D ，返回 any的网卡序号
+     * sagit:/ # tcpdump -D
+     * tcpdump -D
+     * 1.wlan0 [Up, Running]
+     * 2.rmnet_ipa0 [Up, Running]
+     * 3.dummy0 [Up, Running]
+     * 4.any (Pseudo-device that captures on all interfaces) [Up, Running]
+     * 5.lo [Up, Running, Loopback]
+     * 6.p2p0 [Up]
+     * 7.nflog (Linux netfilter log (NFLOG) interface)
+     * 8.nfqueue (Linux netfilter queue (NFQUEUE) interface)
+     */
+    private static int tcpdump_d_any() {
+        Log.i("hqr", "tcpdump_d_any start");
+        int num = 1;
+        String[] commands = new String[2];
+        commands[0] = "adb shell";
+        commands[1] = "tcpdump -D";
+        Process process = execCmd(commands);
+        String result = parseInputStream(process.getInputStream());
+        if (!TextUtils.isEmpty(result)) {
+            String[] lines = result.split("\n");
+            for (String line : lines) {
+                if (line.contains("any")) {
+                    Log.i("hqr", "any id = " + line.substring(0, 1));
+                    num = Integer.valueOf(line.substring(0, 1));
+                    break;
+                }
+            }
+        }
+        Log.i("hqr", "tcpdump_d_any end");
+        return num;
+    }
+
+    private static void prepareTCPDumpFile(Context context) {
+        Log.i("hqr", "prepareTCPDumpFile start");
         InputStream is = null;
         OutputStream os = null;
         boolean retVal = false;
@@ -47,36 +124,15 @@ public class CommandsHelper {
             commands[2] = "cp -rf " + dstFile.toString() + " /data/local/tcpdump";
             commands[3] = "rm -r " + dstFile.toString();
             commands[4] = "chmod 777 /data/local/tcpdump";
-            commands[5] = "cd /data/local";
-            commands[6] = "tcpdump -p -vv -s 0 -w " + DEST_FILE;
-            execCmd(commands);
+            commands[5] = "exit";
+            execCmd(commands, false);
         } catch (IOException e) {
             e.printStackTrace();
-            Log.i(TAG, " error: " + e.getMessage());
         } finally {
             closeSafely(is);
             closeSafely(os);
         }
-        return retVal;
-    }
-
-    public static void stopCapture(Context context) {
-        // 找出所有的带有tcpdump的进程
-        String[] commands = new String[2];
-        commands[0] = "adb shell";
-        commands[1] = "ps|grep tcpdump|grep root|awk '{print $2}'";
-        Process process = execCmd(commands);
-        String result = parseInputStream(process.getInputStream());
-        if (!TextUtils.isEmpty(result)) {
-            String[] pids = result.split("\n");
-            if (null != pids) {
-                String[] killCmds = new String[pids.length];
-                for (int i = 0; i < pids.length; ++i) {
-                    killCmds[i] = "kill -9 " + pids[i];
-                }
-                execCmd(killCmds);
-            }
-        }
+        Log.i("hqr", "prepareTCPDumpFile end");
     }
 
     public static Process execCmd(String command) {
@@ -113,7 +169,7 @@ public class CommandsHelper {
                     retval = false;
                 }
             } catch (Exception ex) {
-                Log.w("Error ejecutando el comando Root", ex);
+                ex.printStackTrace();
             }
         }
         return suProcess;
